af4a36df8dc66f7ed3fe29890550b4482d25c128
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-05-02 17:47:40 +0200

    add img - img size to fix

--- script.js
+++ script.js
@@ -7,25 +7,47 @@
 // E se volessi un bottone per invertire la "direzione" del carosello?
 
 
-const arrImages = [
-    'assets/img/01.webp',
-    'assets/img/02.webp',
-    'assets/img/03.webp',
-    'assets/img/04.webp',
-    'assets/img/05.webp',
+const images = [
+    {
+        image: 'assets/img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'assets/img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'assets/img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'assets/img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'assets/img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
 ];
 
 const containerHighlighted = document.querySelector('.highlighted');
 const containerThumbs = document.querySelector('.thumbs');
 
-for (let i = 0; i < arrImages.length; i++) {
-    containerHighlighted.innerHTML += `<img src="${arrImages[i]}" alt="" class="${i == 0 ? 'active' : ''}">`;
-    containerThumbs.innerHTML += `<img src="${arrImages[i]}" alt="" class="${i == 0 ? 'active' : ''}">`;
+for (let i = 0; i < images.length; i++) {
+	containerHighlighted.innerHTML += 
+    `<div class="img_container ${i == 0 ? 'active' : ''}">
+        <img src="${images[i].image}" alt="" class="${i == 0 ? 'active' : ''}">
+        <h2 class="title">${images[i].title}</h2>
+        <p class="text">${images[i].text}</p>
+    </div>`;
+	containerThumbs.innerHTML += 
+    `<img src="${images[i].image}" alt="" class="${i == 0 ? 'active' : ''}">`;
 }
 
 
-// selezionimo le immagini nell'html
-const listHighlighted = document.querySelectorAll('.highlighted img');
+// selezioniamo le immagini nell'html
+const listHighlighted = document.querySelectorAll('.highlighted .img_container');
 // selezioniamo le miniature
 const listThumbs = document.querySelectorAll('.thumbs img');
 // selezioniamo i bottoni
@@ -38,47 +60,47 @@ const btnNext = document.querySelector('.btn-next');
 let activeIndex = 0;
 
 btnNext.addEventListener('click',
-    function () {
-        // dall'immagine attiva tolgo la classe active
-        listHighlighted[activeIndex].classList.remove('active');
-        listThumbs[activeIndex].classList.remove('active');
-        // settiamo il nuovo valore di active index
-        activeIndex++;
-        if (activeIndex >= listHighlighted.length) {
-            activeIndex = 0;
-        }
-        // alla nuova immagine attiva aggiungiamo la classe active
-        listHighlighted[activeIndex].classList.add('active');
-        listThumbs[activeIndex].classList.add('active');
-    }
+	function() {
+		// dall'immagine attiva tolgo la classe active
+		listHighlighted[activeIndex].classList.remove('active');
+		listThumbs[activeIndex].classList.remove('active');
+		// settiamo il nuovo valore di active index
+		activeIndex++;
+		if (activeIndex >= listHighlighted.length) {
+			activeIndex = 0;
+		}
+		// alla nuova immagine attiva aggiungiamo la classe active
+		listHighlighted[activeIndex].classList.add('active');
+		listThumbs[activeIndex].classList.add('active');
+	}
 );
 
 btnPrev.addEventListener('click',
-    function () {
-        // dall'immagine attiva tolgo la classe active
-        listHighlighted[activeIndex].classList.remove('active');
-        listThumbs[activeIndex].classList.remove('active');
-        // settiamo il nuovo valore di active index
-        activeIndex--;
-        if (activeIndex < 0) {
-            activeIndex = listHighlighted.length - 1;
-        }
-        // alla nuova immagine attiva aggiungiamo la classe active
-        listHighlighted[activeIndex].classList.add('active');
-        listThumbs[activeIndex].classList.add('active');
-    }
+	function() {
+		// dall'immagine attiva tolgo la classe active
+		listHighlighted[activeIndex].classList.remove('active');
+		listThumbs[activeIndex].classList.remove('active');
+		// settiamo il nuovo valore di active index
+		activeIndex--;
+		if (activeIndex < 0) {
+			activeIndex = listHighlighted.length - 1;
+		}
+		// alla nuova immagine attiva aggiungiamo la classe active
+		listHighlighted[activeIndex].classList.add('active');
+		listThumbs[activeIndex].classList.add('active');
+	}
 );
 
 // ciclo per aggiungere gli event listeners alle miniature
 for (let i = 0; i < listThumbs.length; i++) {
-    listThumbs[i].addEventListener('click',
-        function () {
-            console.log('cliccata la miniature in posizione ' + i)
-            listHighlighted[activeIndex].classList.remove('active');
-            listThumbs[activeIndex].classList.remove('active');
-            activeIndex = i;
-            listHighlighted[activeIndex].classList.add('active');
-            listThumbs[activeIndex].classList.add('active');
-        }
-    )
+	listThumbs[i].addEventListener('click',
+		function() {
+			console.log('cliccata la miniature in posizione ' + i)
+			listHighlighted[activeIndex].classList.remove('active');
+			listThumbs[activeIndex].classList.remove('active');
+			activeIndex = i;
+			listHighlighted[activeIndex].classList.add('active');
+			listThumbs[activeIndex].classList.add('active');
+		}
+	)
 }
